7176a1c1f13b3769d024ac2fd849eb6cda2df14d
tvrcgo <tvrcgo@gmail.com>
2015-04-25 16:49:29 +0800

    + merge other hq middlewares; mount other hq;

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # hq
 
-做数据收集，用中件间模式串联任务。
+中件间任务框架。
 
 ## Installation
 
@@ -10,7 +10,7 @@ npm install hq
 
 ## Usage
 
-### use middleware
+### 添加中间件
 
 ```js
 var hq = require('hq');
@@ -18,18 +18,43 @@ var hq = require('hq');
 var h1 = hq();
 
 h1.use(function *(next){
-	//do sth.
     yield next;
+    console.log('h1-1');
 });
 
 h1.use(function *(next){
-	//do sth.
+	console.log('h1-2');
 });
 
 h1.start();
 ```
 
-### mount hq instance
+一次添加多个中间件
+
+```js
+h1.use(function *(next){
+	yield next;
+	console.log('h1-1');
+}, function *(next){
+	console.log('h1-2');
+});
+```
+
+### 合并其它 hq 对象的中间件
+
+将其它 hq 对象的所有中间件合并到当前对象中。
+
+```js
+var h2 = hq();
+
+h2.use(h1);
+
+h2.start();
+```
+
+### 挂载其它 hq 对象
+
+从挂载点进入被挂载对象的中间件，先执行完挂载对象的所有中间件再返回主对象的挂载点继续往下执行。
 
 ```js
 var h1 = hq();
@@ -43,14 +68,12 @@ h1.use(function *(next){
 h2.use(function *(next){
 	yield next;
 	console.log('h2-1');
-});
-
-h2.use(function *(next){
+}, function *(next){
 	console.log('h2-2');
 });
 
-// mount h2
-h1.use(h2);
+// mix instance `h2`
+h1.mix(h2);
 
 h1.use(function *(next){
 	console.log('h1-2');
@@ -66,6 +89,16 @@ h1.start();
 * h1-2
 */
 
+```
+
+### 错误处理
+
+全局错误捕捉，只要一处定义就够了。
+
+```js
+h1.on('error', function(err){
+	console.log(err);
+});
 ```
 
 
--- index.js
+++ index.js
@@ -2,59 +2,93 @@
 var co = require('co');
 var Emitter = require('events').EventEmitter;
 
-/* Constructor */
+/**
+* Constructor 
+*/
 var HQ = module.exports = function(){
 	if (!(this instanceof HQ)) return new HQ;
 	this.middleware = [];
 }
 
-
-/* Prototype */
+/**
+* Prototype 
+*/
 var hq = HQ.prototype;
 
-
-/* Inherit from `Emitter.prototype`. */
+/**
+* Inherit from `Emitter.prototype`. 
+*/
 Object.setPrototypeOf(HQ.prototype, Emitter.prototype);
 
+/**
+* Add middleware 
+*/
+hq.use = function(){
 
-/* Add middleware */
-hq.use = function(mw){
+	var mw = ( arguments.length == 1 ? arguments[0] : [].slice.call(arguments) );
+
+	// use single middleware
 	if ( mw && 'GeneratorFunction' == mw.constructor.name ) {
 		this.middleware.push(mw);
 	}
 
-	// mount other hq
-	if ( mw && mw instanceof HQ) {
+	// use multiple middlewares
+	if ( mw && mw.length && 'Array' == mw.constructor.name ) {
+		this.middleware = mw.concat(this.middleware);
+	}
+
+	// merge other hq middlewares
+	if (mw && mw instanceof HQ) {
 		this.middleware.push(function *(next){
-			yield compose(mw.middleware);
-			yield next;
+			yield *compose(mw.middleware).call(this, next);
 		});
 	}
 
 	return this;
 }
 
+/**
+* Mount hq
+*/
+hq.mix = function(hq){
+	
+	if ( hq && hq instanceof HQ ) {
+		this.middleware.push(function *(next){
+			yield compose(hq.middleware);
+			yield next;
+		});
+	}
+	
+	return this;
+}
 
-/* Process start. */
+/**
+* Process start. 
+*/
 hq.start = function(){
 	var mw = [process].concat(this.middleware);
 	var fn = co.wrap(compose(mw));
-	fn.call(this).catch(this.onerror);
+	var self = this;
+	fn.call(this).catch(function(err){
+		self.emit('error', err);
+	});
 };
 
-
-/* Final process */
+/**
+* Final process 
+*/
 function *process(next){
 	yield *next;
-	// TODO
+	//TODO finally
 }
 
-
-/* Compose middlewares */
+/**
+* Compose middlewares 
+*/
 function compose(middleware){
-	return function *(_next){
+	return function *(next){
 		var i = middleware.length;
-		var next = _next || function *(){};
+		next = next || function *(){};
 		var mw;
 
 		while (i--) {
